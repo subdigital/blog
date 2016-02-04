b8adda3f039666ada6c08f78abcc367bb5b9e2d0
Ben Scheirman <ben@scheirman.com>
2016-02-04 07:54:28 -0600

    fix up permissions for images

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,7 @@ echo "===> Copying to server..."
 echo ""
 rsync -avz public/* benscheirman.com:/srv/www/benscheirman.com/
 
+ssh benscheirman.com -C "chmod 644 /srv/www/benscheirman.com/images/*"
+
 echo ""
 echo "===> Done."
